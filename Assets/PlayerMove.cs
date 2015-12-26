e3fd0221d0e2b5d71a1baaaec4e6c626ad0be73c
Vadim Borodin <borodin.vadim@gmail.com>
2015-12-26 03:30:28 +0300

    initial commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+	void Update()
+	{
+		if (isLocalPlayer) {
+			var x = Input.GetAxis("Horizontal") * 0.1f;
+			var z = Input.GetAxis("Vertical") * 0.1f;
+
+			transform.Translate(x, 0, z);
+		}
+	}
+}

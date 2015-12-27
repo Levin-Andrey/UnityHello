4132a825dd4eaff6be4f41c69b1d61b2db2b5c3d
Vadim Borodin <borodin.vadim@gmail.com>
2015-12-27 23:02:43 +0300

    debug scene refactoring, prefabs for some tiles, new movement system

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,13 +3,30 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
+	[SyncVar]
+	public Vector3 tilePos;
+
 	void Update()
 	{
+		var dt = Time.deltaTime;
 		if (isLocalPlayer) {
-			var x = Input.GetAxis("Horizontal") * 0.1f;
-			var z = Input.GetAxis("Vertical") * 0.1f;
-
-			transform.Translate(x, 0, z);
+		var speed = 3.0f;
+			var move = new Vector3(
+				Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0,
+				0,
+				Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0
+			).normalized * speed * dt;
+			transform.Translate(move);
+			Vector3 pos = transform.position;
+			pos.x = Mathf.Floor(pos.x + 0.5f);
+			pos.z = Mathf.Floor(pos.z + 0.5f);
+			tilePos = pos;
+		} else {
+			transform.position = Vector3.MoveTowards(
+				transform.position,
+				tilePos,
+				3.0f * dt
+			);
 		}
 	}
 }

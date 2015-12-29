608545fc4488eaac3b8740cf01d57964a78aa546
Vadim Borodin <borodin.vadim@gmail.com>
2015-12-29 22:07:23 +0300

    picking tile with mouse

---
+++ Assets/TilePicker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TilePicker : MonoBehaviour {
+	public GameObject Cursor;
+	public Camera Camera;
+	Plane ground;
+
+	void Awake() {
+		ground = new Plane(Vector3.up, new Vector3(0, 0, 0));
+	}
+	
+	void Update() {
+		Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+		float rayDistance;
+		if (ground.Raycast(ray, out rayDistance)) {
+			Cursor.transform.position = GridManager.PosToTile(ray.GetPoint(rayDistance));
+		}
+	}
+}

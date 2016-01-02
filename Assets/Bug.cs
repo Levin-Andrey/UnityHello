ddbbe8ee6c4dd7290e2cd3ba464fafa165c259a5
U-lenny-THINK\lenny <lennytmp@gmail.com>
2016-01-03 03:40:18 +0400

    adding bugs

---
+++ Assets/Bug.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bug : MonoBehaviour {
+    Transform target;
+
+    public float WalkinSpeed = 1.2f;
+
+	void Update () {
+        var carrots = GameObject.FindGameObjectsWithTag("Carrot");
+        if (target == null) {
+            // TODO: that's just lame
+            foreach (GameObject carrotObject in carrots) {
+                var carrot = carrotObject.GetComponent<Carrot>();
+                target = carrot.transform;
+            }
+        } else {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                GridManager.PosToTile(target.position),
+                WalkinSpeed * Time.deltaTime
+            );
+            var tile = GridManager.PosToTile(transform.position);
+            var tileValue = GridManager.At(tile);
+            if (tileValue != null || tileValue.content != null) {
+                Destroy(tileValue.content);
+                tileValue.content = null;
+                target = null;
+            }
+        }
+    }
+}
---
+++ Assets/BugSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class BugSpawner : NetworkBehaviour
+{
+    public GameObject BugPrefab;
+
+    // Use this for initialization
+    void Start () {
+        var bug = (GameObject)Instantiate(BugPrefab, new Vector3(0, 0), Quaternion.identity);
+        NetworkServer.Spawn(bug);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.value > 0.5) {
+            // TODO: I am am am am ma
+        }
+	}
+}
